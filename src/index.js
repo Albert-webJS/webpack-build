bd3e1b2bd09c4851b228a0bdcb96020f9b500702
Albert Pivnenko <pivnenko.albert@gmail.com>
2023-03-07 07:30:36 +0200

    add style-loader & css-loader, describe rules in webpack config for them

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-window.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("root").innerHTML = "Webpack started!";
-});
+import "./style.css";
+
+const root = document.getElementById("root");
+
+(() => {
+  root.innerHTML = "Webpack started!";
+  root.classList.add("root");
+})();
