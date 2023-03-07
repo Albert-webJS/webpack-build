19cb021a896f5a048355f3c6f242d805896cd467
Albert Pivnenko <pivnenko.albert@gmail.com>
2023-03-07 07:55:03 +0200

    add rules for image in webpack.config

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import "./style.css";
+import Icon from "./icon.jpeg";
 
 const root = document.getElementById("root");
+const icon = new Image();
+icon.src = Icon;
 
 (() => {
   root.innerHTML = "Webpack started!";
   root.classList.add("root");
+
+  root.append(icon);
 })();
